f0bee67066858bcbba2929873a0ec13d0e532c05
Jackson Kolling <jacksonkg97@gmail.com>
2023-09-10 02:03:40 +0000

    Added silver and bronze models, along with the source yml.

---
+++ models/silver/dim_listings_for_tourists.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='table',
+    schema='silver_airbnb') }}
+
+with
+    listings_for_tourists as (
+        select
+            sum(
+                case
+                    when (maximum_nights < 28 and availability_30 < 15) then 1 else 0
+                end
+            ) as popular_listings_for_tourists,
+            sum(
+                case
+                    when (maximum_nights < 28) then 1 else 0
+                end
+            ) as total_listings_for_tourists,
+            room_type
+        from {{ ref('bronze_detailed_listings') }}
+        group by room_type
+    )
+select
+    room_type,
+    popular_listings_for_tourists,
+    total_listings_for_tourists,
+    ROUND(
+        cast(popular_listings_for_tourists as numeric) / cast(total_listings_for_tourists as numeric) * 100, 1
+    ) as ratio_percentage
+from listings_for_tourists
+order by total_listings_for_tourists desc 
